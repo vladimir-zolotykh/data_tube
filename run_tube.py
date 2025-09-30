9df679e65351e1d419cc038571ffb73bfc4b16dd
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-30 19:27:04 +0300

    Add command line args
    --fname-pattern, --search-pattern, --cmd, --log-dir

--- run_tube.py
+++ run_tube.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Generator, TextIO, BinaryIO
+from typing import Generator, TextIO
 import os
 import fnmatch
 import bz2
 import gzip
 import re
+import argparse
+import argcomplete
 
 
 def iter_lognames(
@@ -42,13 +44,33 @@ def filter_lines(file, pattern: str = "(?i)python") -> Generator[str, None, None
             yield line
 
 
+parser = argparse.ArgumentParser(
+    description="""\
+Chain Python generators (like a Unix data pipeline)""",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("--log-dir", help="Top dir of log files tree", default="./www")
+parser.add_argument(
+    "--cmd", choices=["print", "count-bytes"], help="Select command", default="print"
+)
+parser.add_argument("--fname-pattern", help="Select log files", default="access-log*")
+parser.add_argument("--search-pattern", help="Select log lines", default="(?i)python")
+
 if __name__ == "__main__":
-    lognames = iter_lognames()
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    lognames = iter_lognames(args.log_dir, args.fname_pattern)
     files = iter_files(lognames)
     lines = cat_lines(files)
-    # for line in filter_lines(lines):
-    #     print(line)
-    bytes_column = (line.rsplit(None, 1)[1] for line in lines)
-    total: int = sum(int(count) for count in bytes_column if count != "-")
-    # Print total number of transferred bytes in matched lines
-    print("Total: ", total)
+    if args.cmd == "print":
+        for line in filter_lines(lines, args.search_pattern):
+            print(line)
+    elif args.cmd == "count-bytes":
+        bytes_column = (line.rsplit(None, 1)[1] for line in lines)
+        total: int = sum(int(count) for count in bytes_column if count != "-")
+        # Print total number of transferred bytes in matched lines
+        print("Total: ", total)
+    else:
+        raise TypeError(
+            f"{args.cmd}: Invalid command. Expected 'print' or 'count-bytes'"
+        )
